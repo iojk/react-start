f8ba93495d9b5705a08925f73aa36222bd9c56bb
aoponomarev <aoponomarev@ya.ru>
2018-11-21 18:43:26 +0300

    component folders

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Message = props => <div>{props.msg}</div>;
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <div>
+            <Message msg="A Generic Hello World Text" />
+            <Message msg="Creating Reusable React Components" />
+        </div>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
